f8f1501c0b5538bddafaab51792403c687e86e2a
Ionut Daniel <ionut.daniel644@gmail.com>
2021-03-03 19:32:38 +0200

    Final Product

--- src/components/other/ProjectFigure.js
+++ src/components/other/ProjectFigure.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ProjectFigure({ language, url, image, name, description }) {
     return (
         <figure className="gallery-grid__item category-concept">
-            <h4 className="title gallery-grid__title">{name}</h4>
+            <h4 className="title gallery-grid__title text-center">{name}</h4>
             <div className="gallery-grid__image-wrap">
                 <a href={url} target="_blank" rel="noreferrer">
                     <img className="gallery-grid__image cover" src={image} data-zoom="" alt="" />
